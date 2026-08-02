96d1c5164d1cdeab4c7885b4cfe1577953effd91
Will Antazo <65074725+willantazo@users.noreply.github.com>
2026-08-02 22:26:31 +0800

    Initial Spring Boot project

---
+++ src/test/java/com/wilatech/travelai/TravelAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilatech.travelai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
