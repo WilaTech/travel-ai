7afcff1419a42c98039593e702dee4b390148693
Will Antazo <65074725+willantazo@users.noreply.github.com>
2026-08-16 17:20:39 +0800

    Add Gemini client and configuration

--- src/main/java/com/wilatech/travelai/client/GeminiClient.java
+++ src/main/java/com/wilatech/travelai/client/GeminiClient.java
@@ -1,11 +1,25 @@
 package com.wilatech.travelai.client;
 
+import com.wilatech.travelai.dto.gemini.Content;
+import com.wilatech.travelai.dto.gemini.GeminiRequest;
+import com.wilatech.travelai.dto.gemini.Part;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Component
+@Primary
 public class GeminiClient implements AIClient {
+
+    private final RestClient geminiRestClient;
+
     @Override
     public String chat(String prompt) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        var request = new GeminiRequest(List.of(new Content(List.of(new Part(prompt)))));
+        return "";
     }
 }
---
+++ src/main/java/com/wilatech/travelai/dto/gemini/Part.java
@@ -0,0 +1,6 @@
+package com.wilatech.travelai.dto.gemini;
+
+public record Part(
+        String text
+) {
+}
